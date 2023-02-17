38562da77cc3444e930507bdc5f551dce9be2bbf
leo-con <56977073+leo-con@users.noreply.github.com>
2023-02-17 15:55:00 -0400

    .

--- site1/js/caso1.js
+++ site1/js/caso1.js
@@ -1,11 +1,5 @@
 var idleTime = 0;
 var idleInterval;
-$(document).ready(function () {
-  ac("dom", "ready", () => {
-    console.log("The journey SDK is ready!");
-    //ac(      "record",      "login",      [{ email: "pedroperez99@hotmail.com", givenName: "Pedro", familyName: "Perez" }],      [{ fieldName: "email" }, { fieldName: "givenName" }, { fieldName: "familyName" }]    );
-  });
-});
 
 function Evento1() {
   ac("record", "ErrorCargarArchivo");
